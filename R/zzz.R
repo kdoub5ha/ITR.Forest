061577c82c263e028d1c5a8648a07046c64c35f0
kdoub5ha <kdoub5ha@gmail.com>
2017-01-31 12:08:10 -0700

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,5 @@
 .onAttach <- function(libname, pkgname){
   options(stringsAsFactors = F)   
-  install.packages("glmnet")
-  install.packages("dplyr")
-  install.packages("RColorBrewer")
-  install.packages("e1071")
   
   library(glmnet)
   library(dplyr)
