972a616d284fc09a1a3ace1df0e1f68100986ec7
kdoub5ha <kdoub5ha@gmail.com>
2017-01-31 11:14:03 -0700

    Update rdat.R

--- R/rdat.R
+++ R/rdat.R
@@ -10,7 +10,7 @@
 #' @param beta1 controls the strength of the treatment effect. Defaults to 2. 
 #' @param beta2 controls the strength of the noise. Defaults to 2. 
 #' @param sigma controls standard deviation of random variation.  Defaults to 1. 
-#' @param cut1/cut2 controls where the cutpoints are to define subgroups. 
+#' @param cut1/cut2/cut3/cut4 controls where the cutpoints are to define subgroups. 
 #' @return dataframe containing y (outcome), x1-x4 (covariates), trt (treatment), prtx (probability of being in treatment group)
 #' @export
 #' @examples
@@ -18,9 +18,10 @@
 #' This generates a dataframe with 500 observations, X1 as the only variable interacting with 
 #' the treatment, and a signal to noise ratio of 2/2=1.
 
-rdat <- function(n=100, K =50, 
-                 beta1=2, beta2=2,
-                 sigma=1, cut1=.5, cut2=.5, depth=1)
+rdat<-function(n=100, K =50, 
+         beta1=2, beta2=2,
+         sigma=1, cut1=.5, cut2=.5, cut3=0.3, cut4=0.1, 
+         depth=1)
 {
   trt <- sample(c(0,1), n, replace=T)
   #### Generate Covariates
@@ -32,7 +33,7 @@ rdat <- function(n=100, K =50,
     mean <- 2 + 2*sign(x1<=cut2) + beta1*sign(x1<=cut1)*trt + beta2*sign(x1>cut1)*(1-trt)
     ##### Output
   }else{ 
-    mean <- 2 + 2*sign(x1<=cut2) + beta1*sign(x1>=0.3 & x3>=0.1)*trt + beta2*(1-sign(x1>=0.3 & x3>=0.1))*(1-trt)
+    mean <- 2 + 2*sign(x1<=cut2) + beta1*sign(x1>=cut3 & x3>=cut4)*trt + beta2*(1-sign(x1>=cut3 & x3>=cut4))*(1-trt)
   } 
   y <- mean + rnorm(n, mean=0, sd=sigma)
   data.frame(x1=x1, x2=x2, x3=x3, x4=x4, y=y, trt=trt,prtx=rep(0.5,n))
