ba6810e05c1880090e2159eaa47b946ebb53f13d
kdoub5ha <kdoub5ha@gmail.com>
2017-05-12 03:32:19 -0700

    Update grow.ITR.R

--- R/grow.ITR.R
+++ R/grow.ITR.R
@@ -11,13 +11,15 @@
 #' @param n0 minimum number of treatment/control observations needed in a split to call a node terminal. Defaults to 5. 
 #' @param max.depth controls the maximum depth of the tree. Defaults to 15. 
 #' @param mtry sets the number of randomly selected splitting variables to be included. Defaults to number of splitting variables.
+#' @param AIPWE logical indicating use of augmented robust estimator 
 #' @return summary of single interaction tree 
 #' @export
 #' @examples
 #' tree<-grow.ITR(data=rctdata, split.var=3:7)
 #' Generates tree using rctdata with potential splitting variables located in columns 3-7.
 
-grow.ITR<-function(data, test=NULL, min.ndsz=20, n0=5, split.var, ctg=NULL, max.depth=15, mtry=length(split.var))
+grow.ITR<-function(data, test=NULL, min.ndsz=20, n0=5, split.var, ctg=NULL, 
+                   max.depth=15, mtry=length(split.var), AIPWE=F)
 {
   # initialize variables.
   out <- NULL
@@ -30,17 +32,53 @@ grow.ITR<-function(data, test=NULL, min.ndsz=20, n0=5, split.var, ctg=NULL, max.
   list.nd <- list(data)
   if (!is.null(test)) list.test <- list(test)
   name <- "0"
+  full.set <- data
   max.score <- NULL
-  full <- data
   # loop over dataset for spliting 
   while (length(list.nd)!=0) {
-    for (i in 1:length(list.nd)){
+    for (i in sample(1:length(list.nd), size = length(list.nd), replace = F)){
       if (!is.null(dim(list.nd[[i]])) && nrow(list.nd[[i]]) > 1){
         test0 <- NULL
         if (!is.null(test)) test0 <- list.test[[i]]
         if(length(list.nd)>1) {temp.tree=temp.tree} else{temp.tree <- NULL}
+        
+        # inilize score statistics. 
+        # at the initial stage, there is no subgroup. 
+        # Inidividuals either assign to trt=1 (z=rep(1,dim(dat)[1]))  
+        # or trt=0 (z=rep(0,dim(dat)[1])) depending on which one gives better utility.
+        # This sets the default utility as the max of all patients in treatment or all in control
+        if(name[i]=="0"){
+          max.score <- max(itrtest(data, z=rep(0,nrow(data)), n0, AIPWE),itrtest(data, z=rep(1,nrow(data)), n0, AIPWE))
+        }else{
+          send<-send.down(data, temp.tree)
+          node<-substr(send$data$node,1,nchar(send$data$node)-1)
+          direction<-substr(send$data$node,nchar(send$data$node),nchar(send$data$node))
+          trt.dir <- temp.tree[match(node,temp.tree$node),]$cut.1
+          
+          trt.pred<-ifelse(trt.dir=="r" & direction=="1",0,
+                           ifelse(trt.dir=="r" & direction=="2",1,
+                                  ifelse(trt.dir=="l" & direction=="1",1,0)))
+          
+          max.score <- itrtest(dat = data, z=trt.pred, n0, AIPWE)
+          
+          #Obtain treatments for those not included in the node
+          #if(length(list.nd)>1) dat<-dat[,-which(colnames(dat)=="new.trt")]
+          send<-dat.rest<-node<-direction<-trt.dir<-trt.pred<-NULL
+          dat.rest <- data[!is.element(data$id, list.nd[[i]]$id),]
+          send<-send.down(dat.rest, temp.tree)
+          node<-substr(send$data$node,1,nchar(send$data$node)-1)
+          direction<-substr(send$data$node,nchar(send$data$node),nchar(send$data$node))
+          trt.dir <- temp.tree[match(node,temp.tree$node),]$cut.1
+          
+          trt.pred<-ifelse(trt.dir=="r" & direction=="1",0,
+                           ifelse(trt.dir=="r" & direction=="2",1,
+                                  ifelse(trt.dir=="l" & direction=="1",1,0)))
+          dat.rest$trt.new<-trt.pred
+        }
+        
         # Determine best split across all covariates
-        split <- partition.ITR(dat = list.nd[[i]], test = test0, name = name[i], min.ndsz=min.ndsz,n0=n0, split.var=split.var, ctg=ctg, max.depth=max.depth, mtry=mtry, temp.tree=temp.tree, full=full)
+        split <- partition.ITR(dat = list.nd[[i]], test = test0, name = name[i], min.ndsz=min.ndsz,n0=n0, split.var=split.var, ctg=ctg, 
+                               max.depth=max.depth, mtry=mtry, dat.rest=dat.rest, max.score=max.score, AIPWE = AIPWE)
         out <- rbind(out, split$info)
         if(!is.null(nrow(split$left))&&!is.null(nrow(split$right))){
           min.n <- min(nrow(split$left),nrow(split$right))
@@ -55,6 +93,7 @@ grow.ITR<-function(data, test=NULL, min.ndsz=20, n0=5, split.var, ctg=NULL, max.
           temp.test <- c(temp.test, list(split$left.test, split$right.test))
         }
       }
+      temp.tree <- out
     }
     list.nd <- temp.list
     list.test <- temp.test
@@ -67,16 +106,13 @@ grow.ITR<-function(data, test=NULL, min.ndsz=20, n0=5, split.var, ctg=NULL, max.
   out$node <- as.character(out$node)
   out <- out[order(out$node), ]
   for(p in 1:dim(out)[1]) {out$var[p]<-ifelse(is.na(de(out$node[p],out)[1]),NA,out$var[p])}
-  for(p in 1:dim(out)[1]) {out$vname[p]<-ifelse(is.na(de(out$node[p],out)[1]),NA,out$vname[p])}
-  for(p in 1:dim(out)[1]) {out$cut.1[p]<-ifelse(is.na(de(out$node[p],out)[1]),NA,out$cut.1[p])}
-  for(p in 1:dim(out)[1]) {out$cut.2[p]<-ifelse(is.na(de(out$node[p],out)[1]),NA,out$cut.2[p])}
-  for(p in 1:dim(out)[1]) {out$score[p]<-ifelse(is.na(de(out$node[p],out)[1]),NA,out$score[p])}
+  for(p in 1:nrow(out)) {if(is.na(out$var[p])) out$vname[p] <- out$cut.1[p] <- out$cut.2[p] <- out$score[p] <- NA}
   if(!is.null(test)){
-    for(p in 1:dim(out)[1]) {out$score.test[p]<-ifelse(is.na(out$var[p]),NA,out$score.test[p])}
+    for(p in 1:nrow(out)) {out$score.test[p]<-ifelse(is.na(out$var[p]),NA,out$score.test[p])}
   }
   if(!is.null(test)){
-  pruned<-prune(tre = out, a=0, train=data, test=test)
-  out$score.test <- as.numeric(pruned$Va.test[match(out$node, pruned$node.rm)])
+    pruned<-prune(tre = out, a=0, train=data, test=test)
+    out$score.test <- as.numeric(pruned$Va.test[match(out$node, pruned$node.rm)])
   }
   out
 }
