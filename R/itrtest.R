dc2c774fa447149e67d4e6c33d72798787124671
kdoub5ha <kdoub5ha@gmail.com>
2017-05-12 03:33:25 -0700

    Update itrtest.R

--- R/itrtest.R
+++ R/itrtest.R
@@ -4,20 +4,32 @@
 #' @param dat dataset being assessed 
 #' @param z new (alternative) treatment assignment in the splitting procedure.  
 #' @param n0 minimum number of observations needed to make a split. 
+#; @param aug logical indicates use of augmented robust estimator
 #' @return itr value from a defined split.
 #' @export
 
 
 
-itrtest <- function(dat,z,n0){
+itrtest <- function(dat,z,n0,aug){
   y <- dat$y
   trt <- dat$trt
   prtx <- dat$prtx
+  if(aug){
+    if(length(trt==1)>0 & length(trt==0)>0){
+    m1 <- mean(y[trt==1])
+    m0 <- mean(y[trt==0])
+    }
+  }
   itr <- NA
   n <- nrow(dat)
   if (length(y)!=length(z)) stop("the vector z must have the same length as data.")
-  if(n > n0) {
-    itr <- mean(trt*y*z/prtx+(1-trt)*y*(1-z)/(1-prtx))/mean(trt*z/prtx+(1-trt)*(1-z)/(1-prtx))
-  }
+  if(min(sum(trt), sum(1-trt)) > n0) {
+    if(!aug) itr <- mean(trt*y*z/prtx+(1-trt)*y*(1-z)/(1-prtx))/mean(trt*z/prtx+(1-trt)*(1-z)/(1-prtx))
+    if(aug){
+     first <- (trt*z+(1-trt)*(1-z))*y/(prtx*trt+(1-prtx)*(1-trt))
+     second <- (((trt*z+(1-trt)*(1-z)) - (prtx*trt+(1-prtx)*(1-trt)))/((prtx*trt+(1-prtx)*(1-trt))))*(m1*z+m0*(1-z))
+     itr <- mean(first-second)
+    }
+}
   itr
 }
