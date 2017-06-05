b4cc9e75104611016b1fd7ce56e5355f388b0af9
kdoub5ha <kdoub5ha@gmail.com>
2017-06-05 12:36:29 -0700

    Add files via upload

---
+++ R/power.set.R
@@ -0,0 +1,27 @@
+#' Used for categorical predictor subsetting in the tree growing function. 
+#' 
+#' @param x a categorical predictor. 
+#' @return Returns a list of the subsets of the predictor to be considered. 
+#' @export
+
+power.set <- function(x) {
+  if(length(x) == 0)
+    return(vector(mode(x), 0))
+  x <- sort(unique(x))
+  n <- length(x)
+  K <- NULL
+  for(m in x) K <- rbind(cbind(K, F), cbind(K, T))
+  out <- apply(K, 1, function(x, s) s[x], s = x)
+  out <- out[-c(1, length(out))]
+  l <- length(out)
+  i <- 1
+  while (i <= l) {
+    if (length(out[[i]]) >= ceiling(n/2+.5)) {
+      out[[i]] <- NULL
+      i <- i-1
+    }
+    i <- i+1 
+    l <- length(out) 
+  }
+  out
+}  
