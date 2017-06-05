0d8404153af5e8ef985e3d07a7ee43e62b2bcb0a
kdoub5ha <kdoub5ha@gmail.com>
2017-06-05 05:55:03 -0700

    Update itrtest.R

--- R/itrtest.R
+++ R/itrtest.R
@@ -14,6 +14,7 @@ itrtest <- function(dat,z,n0,aug){
   y <- dat$y
   trt <- dat$trt
   prtx <- dat$prtx
+
   if(aug){
     if(length(trt==1)>0 & length(trt==0)>0){
     m1 <- mean(y[trt==1])
@@ -23,13 +24,13 @@ itrtest <- function(dat,z,n0,aug){
   itr <- NA
   n <- nrow(dat)
   if (length(y)!=length(z)) stop("the vector z must have the same length as data.")
-  if(min(sum(trt), sum(1-trt)) > n0) {
+  #if(min(sum(trt), sum(1-trt)) >= n0) {
     if(!aug) itr <- mean(trt*y*z/prtx+(1-trt)*y*(1-z)/(1-prtx))/mean(trt*z/prtx+(1-trt)*(1-z)/(1-prtx))
     if(aug){
      first <- (trt*z+(1-trt)*(1-z))*y/(prtx*trt+(1-prtx)*(1-trt))
      second <- (((trt*z+(1-trt)*(1-z)) - (prtx*trt+(1-prtx)*(1-trt)))/((prtx*trt+(1-prtx)*(1-trt))))*(m1*z+m0*(1-z))
      itr <- mean(first-second)
-    }
+    #}
 }
-  itr
+  return(round(itr,4))
 }
